dbe318d07716edc29ce854a037a48261548524d1
Shehu Muhammad <shehumuhammad@my.ccsu.edu>
2019-06-03 19:23:06 -0400

    Add js to make choices turn green/red

--- game.js
+++ game.js
@@ -72,8 +72,19 @@ choices.forEach(choice =>{
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset["number"];
-        console.log(selectedAnswer);
-        getNewQuestion();
+        
+        // const classToApply = 'incorrect';
+        // if(selectedAnswer === currentQuestion.answer){
+        //     classToApply = 'correct';
+        // }
+
+        const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+        
+        selectedChoice.parentElement.classList.add(classToApply);
+        setTimeout(()=>{
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
     });
 });
 
